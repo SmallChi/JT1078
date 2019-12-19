18231e721bfc2bf177a9ef9abb64bcc614899780
SmallChi(Koike) <564952747@qq.com>
2019-12-19 13:05:53 +0800

    增加Amf3Metadata接口属性注释

--- src/JT1078.Flv/Metadata/IAmf3Metadata.cs
+++ src/JT1078.Flv/Metadata/IAmf3Metadata.cs
@@ -6,9 +6,22 @@ namespace JT1078.Flv.Metadata
 {
     public interface IAmf3Metadata
     {
+        /// <summary>
+        /// 字段长度
+        /// </summary>
         ushort FieldNameLength { get; set; }
+        /// <summary>
+        /// 字段名称
+        /// </summary>
         string FieldName { get; set; }
+        /// <summary>
+        /// Amf3数据类型
+        /// ref:video_file_format_spec_v10.pdf scriptdatavalue  type
+        /// </summary>
         byte DataType { get; set; }
+        /// <summary>
+        /// 对应的值
+        /// </summary>
         object Value { get; set; }
         ReadOnlySpan<byte> ToBuffer();
     }
